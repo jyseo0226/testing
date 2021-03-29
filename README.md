d4a117798b107c3261aa64b763c63f3bb7ecf6ea
Jinyoung Seo <jinyoungseo@campus-004-212.ucdavis.edu>
2021-03-29 14:10:53 -0700

    cc

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A repository that will be linked with RStudio
 
 1
+2
---
+++ TestingR.R
@@ -0,0 +1,7 @@
+print("This file was created within RStudio")
+
+print("And not it lives on GitHub")
+
+print("Another line")
+
+print("Testing commit")
